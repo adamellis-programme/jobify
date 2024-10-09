5459ba568d425109ae5623d93825fc3ce93afd46
adamEllisMacBook <125833852+adamEllisMacBook@users.noreply.github.com>
2024-10-09 15:12:47 +0100

    second commit

--- models/jobModel.js
+++ models/jobModel.js
@@ -29,9 +29,4 @@ const JobSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
-// collection is like a table
-// here is where we name the schema Job
-// we export this as we bring it into the controllers
-// Job is created at jobs and then the schema is provided
-// if the property is not listed here and try to add in then it wil get quietly ignored
 export default mongoose.model('Job', JobSchema)
